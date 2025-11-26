a44782ceb8eec4fe219e68f29f91ae7e3f74aba8
Elysium853 <201851659+Elysium853@users.noreply.github.com>
2025-11-26 00:23:55 +0000

    1st batch added

---
+++ modules/getRandomArrayElement.js
@@ -0,0 +1,9 @@
+function getRandomArrayElement(arr) {
+    if (!Array.isArray(arr) || arr.length === 0) {
+        return undefined;
+    }
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+}
+
+module.exports = getRandomArrayElement;
